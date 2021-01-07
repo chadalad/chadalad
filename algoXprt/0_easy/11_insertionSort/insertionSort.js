4d42b7dc72c7417e8a394ee5787a04495ed4aafd
chadalad <cnuckols7@gmail.com>
2021-01-06 22:47:52 -0500

    O(n) best, O(n^2) average and wrst, O(1) space

---
+++ algoXprt/0_easy/11_insertionSort/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            swap(j, j - 1, array);
+            j -= 1;
+        }
+    }
+    return array;
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}

8fee56fcc7a83df16a206290f04394727126b900
chadalad <cnuckols7@gmail.com>
2021-01-07 23:23:52 -0500

    O(n^2) time as best, average, and worst. O(1) space.

---
+++ algoXprt/0_easy/12_selectionSort/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(array) {
+    let startIdx = 0;
+    while (startIdx < array.length - 1) {
+        let smallestIdx = startIdx;
+        for (let i = startIdx + 1; i < array.length; i++) {
+            if (array[smallestIdx] > array[i]) smallestIdx = i;
+        }
+        swap(startIdx, smallestIdx, array);
+        startIdx++;
+    }
+    return array;
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}

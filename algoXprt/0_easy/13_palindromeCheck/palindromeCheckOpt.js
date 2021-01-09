89cd8ac954112fed28705deed45617602b5fdfb2
chadalad <cnuckols7@gmail.com>
2021-01-08 19:54:10 -0500

    optimized to O(n) time and O(n) space

---
+++ algoXprt/0_easy/13_palindromeCheck/palindromeCheckOpt.js
@@ -0,0 +1,7 @@
+function isPalindrome(string) {
+    const reversedChars = [];
+    for (let i = string.length - 1; i >=0 ; i--) {
+        reversedChars.push(string[i]);
+    }
+    return string === reversedChars.join('');
+}

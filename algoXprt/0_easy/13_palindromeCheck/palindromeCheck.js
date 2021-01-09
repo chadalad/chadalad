2b392b5e4aa29e2d1ea4b3894e17277d5bba203a
chadalad <cnuckols7@gmail.com>
2021-01-08 19:47:43 -0500

    O(n^2) time and O(n) space

---
+++ algoXprt/0_easy/13_palindromeCheck/palindromeCheck.js
@@ -0,0 +1,7 @@
+function isPalindrome(string) {
+    let reversedString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return string === reversedString;
+}

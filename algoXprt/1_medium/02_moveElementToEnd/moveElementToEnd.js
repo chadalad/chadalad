c9e813bbf0ef94885b3816dfc070e031f7453638
chadalad <cnuckols7@gmail.com>
2021-01-15 15:31:31 -0500

    O(n) time and O(1) space

---
+++ algoXprt/1_medium/02_moveElementToEnd/moveElementToEnd.js
@@ -0,0 +1,16 @@
+function moveElementToEnd(array, toMove) {
+    let i = 0;
+    let j = array.length - 1;
+    while (i < j) {
+        while (i < j && array[j] === toMove) j--;
+        if (array[i] === toMove) swap(i, j, array);
+        i++;
+    }
+    return array;
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}

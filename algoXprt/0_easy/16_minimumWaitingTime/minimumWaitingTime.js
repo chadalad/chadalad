4179daf767a531afda37d3c090967e89c93dea45
chadalad <cnuckols7@gmail.com>
2021-02-02 20:57:00 -0500

    minimum waiting time with O(nlogn) time and O(1) space

---
+++ algoXprt/0_easy/16_minimumWaitingTime/minimumWaitingTime.js
@@ -0,0 +1,12 @@
+function minimumWaitingTime(queries) {
+    queries.sort((a, b) => a - b);
+
+    let totalWaitingTime = 0;
+    for (let idx = 0; idx < queries.length; idx++) {
+        const duration = queries[idx];
+        const queriesLeft = queries.length - (idx + 1);
+        totalWaitingTime += duration * queriesLeft;
+    }
+
+    return totalWaitingTime;
+}

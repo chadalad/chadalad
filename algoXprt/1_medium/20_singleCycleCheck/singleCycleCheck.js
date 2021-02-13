4bc7ffb3808cfdbeb7aa063438b7914aae9eda2c
chadalad <cnuckols7@gmail.com>
2021-02-13 17:41:59 -0500

    single cycle check with O(n) time and O(1) space

---
+++ algoXprt/1_medium/20_singleCycleCheck/singleCycleCheck.js
@@ -0,0 +1,16 @@
+function hasSingleCycle(array) {
+    let numElementsVisited = 0;
+    let currentIdx = 0;
+    while (numElementsVisited < array.length) {
+        if (numElementsVisited > 0 && currentIdx === 0) return false;
+        numElementsVisited++;
+        currentIdx = getNextIdx(currentIdx, array);
+    }
+    return currentIdx === 0;
+}
+
+function getNextIdx(currentIdx, array) {
+    const jump = array[currentIdx];
+    const nextIdx = (currentIdx + jump) % array.length;
+    return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}

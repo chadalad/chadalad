25d6d7a37917ca7ad983e4cb757710ebfa24fb20
chadalad <cnuckols7@gmail.com>
2021-03-01 23:11:52 -0500

    height balanced binary tree with O(n) time and O(h) space

---
+++ algoXprt/1_medium/32_heightBalancedBinaryTree/heightBalancedBinaryTree.js
@@ -0,0 +1,33 @@
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class TreeInfo {
+    constructor(isBalanced, height) {
+        this.isBalanced = isBalanced;
+        this.height = height;
+    }
+}
+
+function heightBalancedBinaryTree(tree) {
+    const treeInfo = getTreeInfo(tree);
+    return treeInfo.isBalanced;
+}
+
+function getTreeInfo(node) {
+    if (node === null) return new TreeInfo(true, -1);
+
+    const leftSubtreeInfo = getTreeInfo(node.left);
+    const rightSubtreeInfo = getTreeInfo(node.right);
+
+    const isBalanced =
+        leftSubtreeInfo.isBalanced &&
+        rightSubtreeInfo.isBalanced &&
+        Math.abs(leftSubtreeInfo.height - rightSubtreeInfo.height) <= 1;
+    const height = Math.max(leftSubtreeInfo.height, rightSubtreeInfo.height) + 1;
+    return new TreeInfo(isBalanced, height);
+}

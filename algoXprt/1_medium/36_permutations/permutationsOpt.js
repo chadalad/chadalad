90012ee8afc2f68631460de4f7e16cd75e245430
chadalad <cnuckols7@gmail.com>
2021-03-05 18:28:47 -0500

    permutations with O(n^2*n factorial) time and O(n*n factorial) space

---
+++ algoXprt/1_medium/36_permutations/permutationsOpt.js
@@ -0,0 +1,23 @@
+function getPermutations(array) {
+    const permutations = [];
+    permutationsHelper(0, array, permutations);
+    return permutations;
+}
+
+function permutationsHelper(i, array, permutations) {
+    if (i === array.length - 1) {
+        permutations.push(array.slice());
+    } else {
+        for (let j = 1; j < array.length; j++) {
+            swap(i, j, array);
+            permutationsHelper(i + 1, array, permutations);
+            swap(i, j, array);
+        }
+    }
+}
+
+function swap(i, j, array) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}

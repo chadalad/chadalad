85dd6c94cc159c8280245107774cad23d4ce9aa9
chadalad <cnuckols7@gmail.com>
2021-03-14 23:48:07 -0400

    sunset views with O(n) time and O(n) space

---
+++ algoXprt/1_medium/44_sunsetViews/sunsetViewsAlt.js
@@ -0,0 +1,26 @@
+function sunsetViews(buildings, direction) {
+    const candidateBuildings = [];
+
+    const startIdx = direction === 'EAST' ? 0 : buildings.length - 1;
+    const step = direction === 'EAST' ? 1 : -1;
+
+    let idx = startIdx;
+    while (idx >= 0 && idx < buildings.length) {
+        const buildingHeight = buildings[idx];
+
+        while (
+            candidateBuildings.length > 0 &&
+            buildings[candidateBuildings[candidateBuildings.length - 1]] < buildingHeight
+        ) {
+            candidateBuildings.pop();
+        }
+
+        candidateBuildings.push(idx);
+
+        idx = idx + step;
+    }
+
+    if (direction === 'WEST') candidateBuildings.reverse();
+
+    return candidateBuildings;
+}
